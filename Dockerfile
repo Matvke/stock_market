580d01ac112999440786fcd59f23508359239b61
Matvke <matvei31055@gmail.com>
2025-04-28 17:44:16 +0500

    docker stuff

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.12.6-slim-bookworm-20240218
+FROM python:3.12-slim-bookworm
 
 WORKDIR /app
 
@@ -10,4 +10,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["python", "-m", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- config.py
+++ config.py
@@ -1,36 +1,24 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing import ClassVar
-
 
 class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
-    DB_HOST: str = "postgres"
-    DB_PORT: int = 5432
 
-    base_dir: ClassVar[str] = os.path.dirname(os.path.abspath(__file__))
-    secrets_dir: ClassVar[str] = os.path.join(base_dir, "secrets")
-    DB_HOST: str = "postgres"
-    DB_PORT: int = 5432
-    
-    # model_config = SettingsConfigDict(
-    #     env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
-    # )
-    model_config = SettingsConfigDict(env_file=None)
-    
-    def get_db_url(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    model_config = SettingsConfigDict(
 
-
-def load_secrets_from_files():
-    secrets_path = Settings.secrets_dir
-    return dict(
-        DB_USER=open(os.path.join(secrets_path, "db_user.secret")).read().strip(),
-        DB_PASSWORD=open(os.path.join(secrets_path, "db_password.secret")).read().strip(),
-        DB_NAME=open(os.path.join(secrets_path, "db_name.secret")).read().strip(),
+        env_file=".env" if os.path.exists(".env") else None,
+        env_file_encoding='utf-8',
+        extra='ignore' 
     )
 
-        
-settings = Settings(**load_secrets_from_files())
+    def get_db_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+
+settings = Settings()
